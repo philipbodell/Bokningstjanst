f47462f31ce6bc24acee7400ca170e059e4255c6
Patrik Fritzner <Patrik@>
2013-10-17 11:38:52 +0200

    added session conversation

--- Frontend/src/main/java/com/web/bokningstjanst/BServlet.java
+++ Frontend/src/main/java/com/web/bokningstjanst/BServlet.java
@@ -58,6 +58,14 @@ public class BServlet extends HttpServlet {
                     case "Confirm":   
                        HttpSession session =  request.getSession();
                        session.setMaxInactiveInterval(30);
+                       
+                       session.setAttribute("depature_city", request.getAttribute("depature_cit"));
+                       session.setAttribute("depature_time", request.getAttribute("depature_time"));
+                       session.setAttribute("depature_date", request.getAttribute("depature_date"));
+                       session.setAttribute("arrival_city", request.getAttribute("arrival_city"));
+                       session.setAttribute("arrival_date", request.getAttribute("arrival_date"));
+                       session.setAttribute("arrival_time", request.getAttribute("arrival_time"));
+                       
                        request.getRequestDispatcher("WEB-INF/jsp/ticket/ticketValidation.jspx").forward(request, response);
                        
                    
